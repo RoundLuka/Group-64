847cad8728572a82de7422275f81168a8598e85a
RoundLuka <lukagurgenidze282007@gmail.com>
2026-01-25 23:15:30 +0400

    commit

---
+++ Level 98/Classwork/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+import './App.css'
+import Message from "./Meesage";
+
+
+function App() { 
+  return (
+    <>
+      <Message />
+      <p className="greetMessage">ewtger</p>
+    </>
+
+  )
+}
+
+export default App;
