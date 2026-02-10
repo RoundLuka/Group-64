c879ff936d63bbb5a0fd408f61c94b48d7dc3e06
RoundLuka <lukagurgenidze282007@gmail.com>
2026-02-10 23:59:33 +0400

    commit

---
+++ Level 102/hooks/useToggle.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+
+const useToggle = (initialState = false) => {
+    const [state, setState] = useState(initialState);
+
+    const toggle = () => { setState(state => !state) }
+
+    return [state, toggle]
+}
+
+
+export default useToggle;
