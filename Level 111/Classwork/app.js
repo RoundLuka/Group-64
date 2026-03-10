fdc899e5efc62c58cd34fd57b9c68599c8cffe30
RoundyLuka <lukagurgenidze282007@gmail.com>
2026-03-10 23:48:24 +0400

    commit

---
+++ Level 111/Classwork/app.js
@@ -0,0 +1,26 @@
+// const http = require("http")
+// const express = require("express")
+
+// const app = express()
+
+// // routes
+
+
+// app.listen(3000, () => {
+//     console.log("Listening to port 3000")
+// })
+
+
+// setTimeout(() => {
+//     console.log("hello world")
+// }, 2000);
+
+// for(let i = 0; i < 10; i++) {
+//     console.log("etwery")
+// }
+
+// if (true) {
+//     console.log(true)
+// }
+
+console.log(global.process)
